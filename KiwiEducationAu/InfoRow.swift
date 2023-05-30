073977098f17680f8064c2777091a7f1a9cdce25
Aleksey Nebesniy <103752883+ACID-Sky@users.noreply.github.com>
2023-05-30 12:53:08 +0300

    Выполнил задание 1 по ДЗ SwiftUI1

---
+++ KiwiEducationAu/InfoRow.swift
@@ -0,0 +1,38 @@
+//
+//  InfoRow.swift
+//  KiwiEducationAu
+//
+//  Created by Лёха Небесный on 30.05.2023.
+//
+
+import SwiftUI
+
+struct InfoRow: View {
+    var post: Post
+
+    var body: some View {
+        VStack {
+            HStack {
+                post.image
+                    .resizable()
+                    .scaledToFill()
+                    .clipShape(Circle())
+                    .frame(width: 60, height: 60)
+                    .padding(.leading, 12)
+
+                Text(post.title)
+                    .font(.headline)
+
+                Spacer()
+            }
+            Text(post.subtitle)
+                .font(.subheadline)
+        }
+    }
+}
+
+struct InfoRow_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoRow(post: posts[0])
+    }
+}
