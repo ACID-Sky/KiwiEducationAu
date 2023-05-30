ee4b4eadd6dad64cfc1918e554fb00dea4564d00
Aleksey Nebesniy <103752883+ACID-Sky@users.noreply.github.com>
2023-05-30 10:31:54 +0300

    Initial Commit

---
+++ KiwiEducationAu/KiwiEducationAuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiwiEducationAuApp.swift
+//  KiwiEducationAu
+//
+//  Created by Лёха Небесный on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct KiwiEducationAuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
